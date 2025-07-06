1cfe5caccfd63038bb8bf5ea0e4f1e54b4832e09
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-07-06 21:09:51 +0300

    split spec

--- spec.cr
+++ spec.cr
@@ -7,38 +7,37 @@ require "./src/sophia"
 Log.setup :debug
 
 describe Sophia do
-  env = Sophia::Environment.new({
-    "sophia.path"              => "/tmp/sophia",
-    "db"                       => "test",
-    "db.test.compaction.cache" => 4_i64 * 1024 * 1024 * 1024,
-  }.merge Sophia.scheme_conf("test", {"a" => String}, {"b" => String}))
-  describe "Environment" do
-    it "conf" do
-      env.getstring("sophia.path").should eq "/tmp/sophia"
-      env.getint("db.test.compaction.cache").should eq 4_i64 * 1024 * 1024 * 1024
-    end
-  end
   describe "Database" do
+    env = Sophia::Environment.new({
+      "sophia.path"              => "/tmp/sophia",
+      "db"                       => "test",
+      "db.test.compaction.cache" => 4_i64 * 1024 * 1024 * 1024,
+    }.merge Sophia.scheme_conf("test", {"a" => String}, {"b" => String}))
+
     db = Sophia::Database({a: String}, {b: String}).new env, "test"
     a = Random::DEFAULT.hex 8
     b = Random::DEFAULT.hex 8
-    it "CRUD" do
-      # set key=value
+
+    it "sets key=value" do
       db[{a: a}] = {b: b}
+    end
 
-      # get value by key
+    it "get value by key" do
       db[{a: a}]?.should eq({b: b})
+    end
 
-      # iterate from key
+    it "iterate from key" do
       db.from({a: a}, ">=") do |k, v|
         db[k]?.should eq(v)
       end
+    end
 
-      # delete key/value pair
+    it "delete key/value pair" do
       db.delete({a: a})
       db[{a: a}]?.should eq nil
+    end
 
-      # same in transaction
+    it "do all the same in transaction" do
       env.transaction do |tr|
         dbtr = db.in tr
         db[{a: a}] = {b: b}
